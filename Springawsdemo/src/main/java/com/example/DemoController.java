9355508508c4b77e8601cc4f4006281695df5718
BalamanikandanC <balamanikandan.career@gmail.com>
2024-01-11 22:13:52 +0530

    commit

---
+++ Springawsdemo/src/main/java/com/example/DemoController.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+@ResponseBody
+	@RequestMapping("/")
+	public String demo() {
+		return "Hi Leaners welcome to Dockers";
+	}
+	
+	
+}
+
